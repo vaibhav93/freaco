15674adc7616a0359c371853b40540d3ccebfbd6
Vaibhav <vaibhavbansal1993@gmail.com>
2016-01-30 19:45:45 +0530

    manage offer

--- client/admin/assets/js/controllers/rewardsCtrl.js
+++ client/admin/assets/js/controllers/rewardsCtrl.js
@@ -4,9 +4,40 @@
  
  * Simple table with sorting and filtering on AngularJS
  */
-app.controller('rewardsCtrl', ["$scope", "$filter", "$localStorage", "Upload", "ngTableParams", "Business", "$q", "$modal",
-    function($scope, $filter, $localStorage, $upload, ngTableParams, Business, $q, $modal) {
-
+ app.controller('rewardsCtrl', ["$scope", "$filter", "$localStorage", "Upload", "ngTableParams", "Business", "$q", "$modal","usSpinnerService",
+    function($scope, $filter, $localStorage, $upload, ngTableParams, Business, $q, $modal,usSpinnerService) {
+        $scope.offer = {
+            name:null,
+            points:null
+        };
+        $scope.createOffer = function(){
+            usSpinnerService.spin('spinner-1');
+            if($scope.offer.name && $scope.offer.points){
+                Business.offers.create({id:$localStorage.business.id},$scope.offer,function(offer){
+                    $scope.tableParams.reload();
+                    usSpinnerService.stop('spinner-1');
+                    $scope.offer = {
+                        name:null,
+                        points:null
+                    };
+                },function(err){
+                    usSpinnerService.stop('spinner-1');
+                    console.log(err);
+                    $scope.offer = {
+                        name:null,
+                        points:null
+                    };
+                })
+            }
+        }
+        $scope.deleteOffer = function(offerId){
+            usSpinnerService.spin('spinner-2');
+            Business.offers.destroyById({id:$localStorage.business.id,fk:offerId},
+                function(success){console.log('delete success');$scope.tableParams.reload();
+            usSpinnerService.stop('spinner-2');},
+                function(error){console.log('delete error :'+error);
+                usSpinnerService.stop('spinner-2');});
+        }
 
         var promises = [];
         $scope.tableParams = new ngTableParams({
@@ -42,5 +73,5 @@ app.controller('rewardsCtrl', ["$scope", "$filter", "$localStorage", "Upload", "
             }
         });
 
-    }
+}
 ]);
