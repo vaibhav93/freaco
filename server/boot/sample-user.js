8bfe2b8590eeff71a6b04ccd1c7ce9eb99cc7f82
Vaibhav <vaibhavbansal1993@gmail.com>
2016-02-25 20:20:52 +0530

    user name to fname

--- server/boot/sample-user.js
+++ server/boot/sample-user.js
@@ -4,35 +4,45 @@ module.exports = function(app) {
     var userModel = app.models.Appuser;
     var vendorModel = app.models.Vendor;
     var ds = app.dataSources.mongoDs;
-    var sampleVendor = {name:'vendor',email:'vendor@abc.com',password:'vendor'};
+    var sampleVendor = {
+        name: 'vendor',
+        email: 'vendor@abc.com',
+        password: 'vendor'
+    };
     var sample_users = [{
-        name: 'Vaibhav',
-        email:'vaibhav@gmail.com',
-        password:'12345'
+        fname: 'Vaibhav',
+        email: 'vaibhav@gmail.com',
+        password: '12345'
     }, {
-        name: 'Sahil',
-        email:'sahil@gmail.com',
-        password:'12345'
-    },
-    {
-        name: 'Neeraj',
-        email:'neeraj@gmail.com',
-        password:'12345'
+        fname: 'Sahil',
+        email: 'sahil@gmail.com',
+        password: '12345'
+    }, {
+        fname: 'Neeraj',
+        email: 'neeraj@gmail.com',
+        password: '12345'
+    }, {
+        fname: 'Abhishek',
+        email: 'abhishek@gmail.com',
+        password: '12345'
     }]
     ds.once('connected', function() {
-        vendorModel.findOrCreate({where:
-            {email:sampleVendor.email}},sampleVendor,function(err,createdVendor,created){
-                if(err){
-                    console.log(err);
-                }
-                (created ) ? console.log('Created vendor', createdVendor.email) : console.log('Found vendor', createdVendor.email);
-            });
+        vendorModel.findOrCreate({
+            where: {
+                email: sampleVendor.email
+            }
+        }, sampleVendor, function(err, createdVendor, created) {
+            if (err) {
+                console.log(err);
+            }
+            (created) ? console.log('Created vendor', createdVendor.email) : console.log('Found vendor', createdVendor.email);
+        });
         sample_users.forEach(function(user) {
             userModel.findOrCreate({
                 where: {
                     email: user.email
                 }
-            },user, function(err, createdUser, created) {
+            }, user, function(err, createdUser, created) {
                 if (err) {
                     console.log(err)
                 }
