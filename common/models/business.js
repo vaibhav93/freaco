79b3dc3855c2a3b1bd08fc200ba95d8237f6b215
Vaibhav <vaibhavbansal1993@gmail.com>
2016-03-05 17:49:42 +0530

    redeem reward

--- common/models/business.js
+++ common/models/business.js
@@ -8,6 +8,7 @@ module.exports = function(Business) {
          *	businessId (required)
          *  billValue (optional)
          *	type (required)
+         *  offerId (optional) if type REDEEM_REWARD
          */
 
         Business.findById(body.businessId, function(err, business) {
@@ -28,6 +29,16 @@ module.exports = function(Business) {
                                 error: 'No customer'
                             });
                         } else { // customer found. Process according to type of mpin validation
+                            //log activity
+                            var updateActivity = function() {
+                                business.activities.create({
+                                    time: Date.now(),
+                                    customerId: customer.id,
+                                    type: body.type
+                                }, function(err, activity) {
+                                    // console.log(visit);
+                                });
+                            }
                             switch (body.type) {
                                 case 'QR_SCAN':
                                     if (body.billValue) {
@@ -36,6 +47,7 @@ module.exports = function(Business) {
                                             points: business.pointRules.perVisit + customer.points + business.pointRules.billRules.points * (Math.floor(body.billValue / business.pointRules.billRules.value)),
                                             lastVisit: Date.now()
                                         }, function(err, updatedCustomer) {
+                                            updateActivity();
                                             cb(null, updatedCustomer)
                                         })
                                     } else { //no bill value. just update visit points
@@ -44,10 +56,41 @@ module.exports = function(Business) {
                                             points: business.pointRules.perVisit + customer.points,
                                             lastVisit: Date.now()
                                         }, function(err, updatedCustomer) {
+                                            updateActivity();
                                             cb(null, updatedCustomer)
                                         })
                                     }
                                     break;
+                                case 'REDEEM_REWARD':
+                                    var offerModel = app.models.Offer;
+                                    offerModel.findById(body.offerId, function(err, offer) {
+                                        if (err || !offer) {
+                                            var err = new Error();
+                                            err.message = "Invalid offer ID";
+                                            err.status = 801;
+                                            res.statusCode = 200;
+                                            res.send({
+                                                error: err
+                                            });
+                                        } else {
+                                            var updatedPoints = customer.points - offer.points;
+                                            //reduce customer points
+                                            customer.updateAttributes({
+                                                points: updatedPoints
+                                            }, function(err, updatedCustomer) {
+                                                if (err) {
+                                                    err.status = 901; // cannot update customer points
+                                                    res.statusCode = 200;
+                                                    res.send({
+                                                        error: err
+                                                    });
+                                                } else {
+                                                    updateActivity();
+                                                    cb(null, updatedCustomer);
+                                                }
+                                            })
+                                        }
+                                    })
                             }
                         }
                     })
@@ -77,7 +120,7 @@ module.exports = function(Business) {
             }
         }],
         returns: [{
-            arg: 'validity',
+            arg: 'success',
             type: 'object'
         }],
         http: {
