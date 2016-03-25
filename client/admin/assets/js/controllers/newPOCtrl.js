78e7626a18c947f98106e774dac524265b1d306d
Vaibhav <vaibhavbansal1993@gmail.com>
2016-03-26 01:44:24 +0530

    template select

--- client/admin/assets/js/controllers/newPOCtrl.js
+++ client/admin/assets/js/controllers/newPOCtrl.js
@@ -6,50 +6,16 @@
  */
 app.controller('newPOCtrl', ["$scope", "$filter", "$timeout", "Business", "PushOffer", "$localStorage", "Vendor", "$q", "$modal",
     function($scope, $filter, $timeout, Business, PushOffer, $localStorage, Vendor, $q, $modal) {
-        $scope.updateStatus = function(pushOffer) {
-            var status;
-            //pushOffer.status ? status = 'enabled' : status = 'disabled';
-            PushOffer.prototype$updateAttributes({
-                id: pushOffer.id
-            }, {
-                status: pushOffer.status
-            })
+        var currentTemplate = ''
+        $scope.setTemplate = function(template) {
+            console.log(template);
+            currentTemplate = template;
         }
-        var getpushOffers = function() {
-            Business.pushOffers({
-                id: $localStorage.business.id
-            }, function(pushOffers) {
-                $scope.pushOffers = pushOffers;
-                angular.forEach(pushOffers, function(pushOffer) {
-                    PushOffer.customerOffers({
-                        id: pushOffer.id
-                    }, function(customerOffers) {
-                        pushOffer.customerList = customerOffers;
-                    })
-                    Business.activities({
-                            id: $localStorage.business.id,
-                            filter: {
-                                where: {
-                                    redeemId: pushOffer.id
-                                }
-                            }
-                        },
-                        function(activities) {
-                            pushOffer.activities = activities;
-                        })
-                })
-            });
+        $scope.setBackground = function(template) {
+            if (template == currentTemplate) {
+                return 'greenBG';
+            }
         }
-        getpushOffers();
-        $scope.deletePushOffer = function(pushOfferId) {
-            Business.pushOffers.destroyById({
-                id: $localStorage.business.id,
-                fk: pushOfferId
-            }, function(success) {
-                getpushOffers();
-            })
-        }
-        $scope.switchsetting = true;
     }
 ]);
 
