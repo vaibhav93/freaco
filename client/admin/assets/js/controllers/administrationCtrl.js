8dca7aee5bdee590e11225aa92510e6efca1112a
Vaibhav <vaibhavbansal1993@gmail.com>
2016-05-01 23:52:35 +0530

    business profile page

--- client/admin/assets/js/controllers/administrationCtrl.js
+++ client/admin/assets/js/controllers/administrationCtrl.js
@@ -4,8 +4,30 @@
  
  * Simple table with sorting and filtering on AngularJS
  */
-app.controller('administrationCtrl', ["$scope", "$filter", "$timeout", "Business", "$localStorage", "Vendor", "Upload",
-    function($scope, $filter, $timeout, Business, $localStorage, Vendor, $upload) {
+app.controller('administrationCtrl', ["$scope", "$filter", "$timeout", "Business", "$localStorage", "Vendor", "Upload", "SweetAlert",
+    function($scope, $filter, $timeout, Business, $localStorage, Vendor, $upload, SweetAlert) {
+        Business.findById({
+            id: $localStorage.business.id
+        }, function(business) {
+            $scope.business = business;
+        })
+        Business.businessSecurities({
+            id: $localStorage.business.id
+        }, function(BS) {
+            $scope.mpin = BS.mpin;
+        })
+        $scope.saveBusiness = function() {
+            Business.prototype$updateAttributes({
+                id: $scope.business.id
+            }, $scope.business, function(updatedBusiness) {
+                SweetAlert.swal({
+                    title: "Sucess!",
+                    text: "Profile update successfully",
+                    type: "success",
+                    confirmButtonColor: "#007AFF"
+                });
+            })
+        }
         $scope.upload = function(file, num) {
             if (num === 1)
                 $scope.f = file;
